f8ec6274a2f26eb0b4ad9716a1384b89864c0318
ivyroot <jacob@draup.xyz>
2023-03-31 16:17:01 -0700

    initial wip setup

---
+++ src/Collection00.sol
@@ -0,0 +1,13 @@
+import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+
+contract Collection00ERC721 is ERC721 {
+    uint256 public immutable MAX_SUPPLY;
+    string public baseTokenURI;
+
+    constructor(uint256 maxSupply, string memory baseURI) ERC721("COLLECTION 00", "DRAUP:00") {
+        MAX_SUPPLY = maxSupply;
+        baseTokenURI = baseURI;
+    }
+
+}
