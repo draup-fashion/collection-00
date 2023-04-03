6050945b8e789cbef4942a642a825d279e3e9a13
ivyroot <jacob@draup.xyz>
2023-04-03 15:23:18 -0700

    remove scaffold contract

--- test/BasicCase.t.sol
+++ test/BasicCase.t.sol
@@ -5,6 +5,9 @@ import "../src/DRAUPCollection00.sol";
 
 contract BasicCaseTest is Test {
     DRAUPCollection00 public collection;
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    address private owner = vm.addr(uint256(keccak256(abi.encodePacked("owner"))));
+    address private minter = vm.addr(uint256(keccak256(abi.encodePacked("minter"))));
 
     function setUp() public {
         uint256[5] memory initialSupplies;
@@ -14,6 +17,9 @@ contract BasicCaseTest is Test {
         initialSupplies[3] = 88;
         initialSupplies[4] = 256;
         collection = new DRAUPCollection00(initialSupplies, 'https://example.com/yolo/');
+        collection.transferOwnership(owner);
+        vm.deal(owner, 100 ether);
+        vm.deal(minter, 100 ether);
     }
 
     function testGetMaxSupply() public {
