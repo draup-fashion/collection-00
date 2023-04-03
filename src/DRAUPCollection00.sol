1e0dd72b4f96f58f11ff353652c13237d2f4efc3
ivyroot <jacob@draup.xyz>
2023-04-03 15:20:00 -0700

    add initial list of item types

--- src/DRAUPCollection00.sol
+++ src/DRAUPCollection00.sol
@@ -70,34 +70,54 @@ pragma solidity ^0.8.17;
 //
 //
 //
-import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {ERC721A} from "erc721a/contracts/ERC721A.sol";
 import {DefaultOperatorFilterer} from "operator-filter-registry/src/DefaultOperatorFilterer.sol";
 
-
 // define constants for the item types
-uint256 constant CORSET_ITEM_TYPE = 0;
-uint256 constant JACKET_ITEM_TYPE = 1;
+uint256 constant COAT_ITEM_TYPE = 0;
+uint256 constant DRESS_ITEM_TYPE = 1;
+uint256 constant JEANS_ITEM_TYPE = 2;
+uint256 constant CORSET_ITEM_TYPE = 3;
+uint256 constant HAT_ITEM_TYPE = 4;
 
-
-contract DRAUPCollection00 is ERC721, DefaultOperatorFilterer {
+contract DRAUPCollection00 is ERC721A, Ownable, DefaultOperatorFilterer {
     uint256[5] private maxSupplies;
     string public baseTokenURI;
 
-    constructor(uint256[5] memory setSupply, string memory baseURI) ERC721("DRAUP COLLECTION 00", "DRAUP:00") {
+    constructor(uint256[5] memory setSupply, string memory baseURI) ERC721A("DRAUP COLLECTION 00", "DRAUP:00") {
         maxSupplies = setSupply;
         baseTokenURI = baseURI;
     }
 
-    function getMaxTotalSupply() public view returns (uint256) {
+    function getMaxSupply() public view returns (uint256) {
         return maxSupplies[0] + maxSupplies[1] + maxSupplies[2] + maxSupplies[3] + maxSupplies[4];
     }
 
-    function getMaxSupply(uint256 itemType) public view returns (uint256) {
+    function getItemMaxSupply(uint256 itemType) public view returns (uint256) {
         return maxSupplies[itemType];
     }
 
-    function getSupply(uint256 itemType) public view returns (uint256) {
+    function getItemSupply(uint256 itemType) public view returns (uint256) {
         return maxSupplies[itemType];
+    }
+
+    // hero pieces minted by DRAUP using short form generative techniques
+    function mintCoats(address to, bytes32[] calldata seeds) public onlyOwner {
+        uint256 itemType = COAT_ITEM_TYPE;
+        require(maxSupplies[itemType] >= seeds.length, "Not enough supply for minting");
+        maxSupplies[itemType] -= seeds.length;
+        for (uint i=0; i<seeds.length; i++) {
+            _mint(to, itemType);
+        }
+    }
+
+    // main collection pieces minted by public using long form generative techniques
+    // for our random seed strategy see:
+    function mintItems(address to, uint256 itemType) public onlyOwner {
+        require(maxSupplies[itemType] > 0, "Not enough supply for minting");
+        maxSupplies[itemType] -= 1;
+        _mint(to, itemType);
     }
 
     // on-chain royalty enforcement integration
@@ -105,21 +125,22 @@ contract DRAUPCollection00 is ERC721, DefaultOperatorFilterer {
         super.setApprovalForAll(operator, approved);
     }
 
-    function approve(address operator, uint256 tokenId) public override onlyAllowedOperatorApproval(operator) {
+    function approve(address operator, uint256 tokenId) public override payable onlyAllowedOperatorApproval(operator) {
         super.approve(operator, tokenId);
     }
 
-    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+    function transferFrom(address from, address to, uint256 tokenId) public override payable onlyAllowedOperator(from) {
         super.transferFrom(from, to, tokenId);
     }
 
-    function safeTransferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override payable onlyAllowedOperator(from) {
         super.safeTransferFrom(from, to, tokenId);
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data)
         public
         override
+        payable
         onlyAllowedOperator(from)
     {
         super.safeTransferFrom(from, to, tokenId, data);
--- test/BasicCase.t.sol
+++ test/BasicCase.t.sol
@@ -7,14 +7,31 @@ contract BasicCaseTest is Test {
     DRAUPCollection00 public collection;
 
     function setUp() public {
-        collection = new DRAUPCollection00('https://example.com/yolo/');
+        uint256[5] memory initialSupplies;
+        initialSupplies[0] = 8;
+        initialSupplies[1] = 24;
+        initialSupplies[2] = 56;
+        initialSupplies[3] = 88;
+        initialSupplies[4] = 256;
+        collection = new DRAUPCollection00(initialSupplies, 'https://example.com/yolo/');
     }
 
-    function testGetSupply() public {
-        uint256 currentSupply = collection.getSupply(CORSET_ITEM_TYPE);
-        assertEq(currentSupply, 100);
+    function testGetMaxSupply() public {
+        uint256 supply = collection.getMaxSupply();
+        assertEq(supply, 432);
     }
 
+    function testGetItemMaxSupply() public {
+        assertEq(collection.getItemMaxSupply(COAT_ITEM_TYPE), 8);
+        assertEq(collection.getItemMaxSupply(DRESS_ITEM_TYPE), 24);
+        assertEq(collection.getItemMaxSupply(JEANS_ITEM_TYPE), 56);
+        assertEq(collection.getItemMaxSupply(CORSET_ITEM_TYPE), 88);
+        assertEq(collection.getItemMaxSupply(HAT_ITEM_TYPE), 256);
+    }
 
+    function testTotalSupply() public {
+        uint256 supply = collection.totalSupply();
+        assertEq(supply, 0);
+    }
 
 }
