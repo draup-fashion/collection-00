405f7c5ca7ae23419f138cc5e78c1119436ba456
ivyroot <jacob@draup.xyz>
2023-04-03 11:52:40 -0700

    add operator filtering for on-chain enforcement of royalty payments

--- src/DRAUPCollection00.sol
+++ src/DRAUPCollection00.sol
@@ -81,4 +81,29 @@ contract DRAUPCollection00 is ERC721, DefaultOperatorFilterer {
         baseTokenURI = baseURI;
     }
 
+    // on-chain royalty enforcement integration
+    function setApprovalForAll(address operator, bool approved) public override onlyAllowedOperatorApproval(operator) {
+        super.setApprovalForAll(operator, approved);
+    }
+
+    function approve(address operator, uint256 tokenId) public override onlyAllowedOperatorApproval(operator) {
+        super.approve(operator, tokenId);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data)
+        public
+        override
+        onlyAllowedOperator(from)
+    {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+
 }
