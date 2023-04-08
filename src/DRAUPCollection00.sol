162dc89b35410ea568432f62640d5f133dbaebdf
ivyroot <jacob@draup.xyz>
2023-04-08 16:06:29 -0700

    update solidity version to use PREVRANDAO

--- src/DRAUPCollection00.sol
+++ src/DRAUPCollection00.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.19;
 
 //
 // This software is developed by DRAUP, Inc. "DRAUP" is a trademark of DRAUP, Inc.
--- test/BasicCase.t.sol
+++ test/BasicCase.t.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
 import "../src/DRAUPCollection00.sol";
