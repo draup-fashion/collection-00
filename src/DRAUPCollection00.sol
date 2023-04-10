62f9efa72e5972962f88f3bc062fcee4a19eb619
ivyroot <jacob@draup.xyz>
2023-04-10 13:28:29 -0700

    start cleanup now that mints will all be singles

--- src/DRAUPCollection00.sol
+++ src/DRAUPCollection00.sol
@@ -84,25 +84,14 @@ uint constant HAT_ITEM_TYPE = 4;
 // define constants for checking mint transaction validity
 uint8 constant SUCCESS_MINT_ALLOWED = 0;
 uint8 constant ERR_ALLOWLIST_SIGNATURE_INVALID = 1;
-uint8 constant ERR_ITEM_QUANTITY_ZERO = 2;
-uint8 constant ERR_ITEM_QUANTITY_TOO_HIGH = 3;
-uint8 constant ERR_INSUFFICIENT_FUNDS = 4;
-uint8 constant ERR_INSUFFICIENT_ITEM_SUPPLY = 5;
-uint8 constant ERR_CANNOT_MINT_ITEM_TYPE = 6;
+uint8 constant ERR_INSUFFICIENT_FUNDS = 2;
+uint8 constant ERR_INSUFFICIENT_ITEM_SUPPLY = 3;
+uint8 constant ERR_CANNOT_MINT_ITEM_TYPE = 4;
 
 // define constants for mint process
 uint constant MINT_NOT_STARTED = 0;
 uint constant MINT_IN_PROGRESS = 1;
 uint constant MINT_FINISHED = 2;
-
-function splitQuarter(uint256 input) pure returns (uint256[4] memory output) {
-    uint256 mask = 0xffffffffffffffffffffffffffffffff;
-    uint256 quarter = uint256(1) << 256 / 4;
-    output[0] = input & (mask >> 3 * quarter);
-    output[1] = (input >> quarter) & (mask >> 2 * quarter);
-    output[2] = (input >> 2 * quarter) & (mask >> quarter);
-    output[3] = (input >> 3 * quarter) & mask;
-}
 
 contract DRAUPCollection00 is ERC721A, Ownable, DefaultOperatorFilterer {
     using ECDSA for bytes32;
@@ -119,24 +108,22 @@ contract DRAUPCollection00 is ERC721A, Ownable, DefaultOperatorFilterer {
     mapping(uint256 => bytes32) private _tokenSeeds;
 
     constructor(uint256[5] memory setSupply, uint256[5] memory setMintPrices, string memory initialBaseURI, address initialSigner) ERC721A("DRAUP COLLECTION 00", "DRAUP:00") {
-        signer = initialSigner;
         maxSupplies = setSupply;
         mintPrices = setMintPrices;
         baseTokenURI = initialBaseURI;
+        signer = initialSigner;
     }
 
     // Token Info
 
-    function getMaxSupply() public view returns (uint256) {
+    function getMaxSupply() public view returns (uint) {
         return maxSupplies[0] + maxSupplies[1] + maxSupplies[2] + maxSupplies[3] + maxSupplies[4];
     }
 
-    function getItemMaxSupply(uint256 itemType) public view returns (uint256) {
-        return maxSupplies[itemType];
-    }
-
-    function totalItemSupply(uint256 itemType) public view returns (uint256) {
-        return currentSupplies[itemType];
+    function itemSupplyInfo() public view returns (uint[5] memory itemSupplies, uint[5] memory itemMaxSupplies, uint[5] memory itemMintPrices) {
+        itemSupplies = currentSupplies;
+        itemMaxSupplies = itemMaxSupplies;
+        itemMintPrices = mintPrices;
     }
 
     function tokenInfo(uint256 tokenId) public view returns (uint256 itemType, bytes32 seed) {
@@ -186,24 +173,14 @@ contract DRAUPCollection00 is ERC721A, Ownable, DefaultOperatorFilterer {
         _;
     }
 
-    function isValidMintOrder(uint itemType, uint quantity) public view returns (uint) {
+    function validateMintOrder(uint itemType) public view returns (uint) {
         if (itemType == 0 || itemType > 4) {
             return ERR_CANNOT_MINT_ITEM_TYPE;
         }
-        if (quantity == 0) {
-            return ERR_ITEM_QUANTITY_ZERO;
-        }
-        if (quantity > 4) {
-            return ERR_ITEM_QUANTITY_TOO_HIGH;
-        }
-        if (currentSupplies[itemType] + quantity > maxSupplies[itemType]) {
+        if (currentSupplies[itemType] == 0) {
             return ERR_INSUFFICIENT_ITEM_SUPPLY;
         }
         return SUCCESS_MINT_ALLOWED;
-    }
-
-    function mintCostForItems(uint itemType, uint quantity) public view returns (uint totalCost) {
-        totalCost = mintPrices[itemType] * quantity;
     }
 
     // hero pieces minted by DRAUP using short form generative techniques
@@ -222,42 +199,26 @@ contract DRAUPCollection00 is ERC721A, Ownable, DefaultOperatorFilterer {
     }
 
     // main collection pieces minted by public using long form generative techniques
-    function mintItems(address to, uint itemType, uint quantity, bytes calldata signature) public payable onlyDuringMinting {
-        uint mintOrderStatus = isValidMintOrder(itemType, quantity);
+    function mintItem(uint itemType, bytes calldata signature) public payable onlyDuringMinting {
+        // validate mint order
+        uint mintOrderStatus = validateMintOrder(itemType);
         if (mintOrderStatus != SUCCESS_MINT_ALLOWED) {
             revert MintValidationFailure(mintOrderStatus);
         }
-        // check that the signature is valid
         if (!isValidSignature(msg.sender, signature)) {
             revert MintValidationFailure(ERR_ALLOWLIST_SIGNATURE_INVALID);
         }
-        uint totalCost = mintCostForItems(itemType, quantity);
-        if (msg.value < totalCost) {
+        if (msg.value < mintPrices[itemType]) {
             revert MintValidationFailure(ERR_INSUFFICIENT_FUNDS);
         }
-        // mint the tokens
-        currentSupplies[itemType] += quantity;
-        uint batchStartTokenId = _nextTokenId();
-        uint rand = block.prevrandao;
-        for (uint i=0; i<quantity; i++) {
-            if (quantity > 1) {
-                uint[4] memory rquarters = splitQuarter(rand);
-                if (quantity == 2) {
-                    if (i == 0) {
-                        rand = rquarters[0] | rquarters[1];
-                    } else {
-                        rand = rquarters[2] | rquarters[3];
-                    }
-                } else {
-                    // NB: when batch size is 3 we drop the randomness contribution from the last quarter of randao
-                    rand = rquarters[i];
-                }
-            }
-            bytes32 seed = keccak256(abi.encodePacked(batchStartTokenId+i, rand, blockhash(block.number - 1), msg.sender));
-            _tokenItemTypes[batchStartTokenId+i] = itemType;
-            _tokenSeeds[batchStartTokenId+i] = seed;
-        }
-        _mint(to, quantity);
+
+        // mint token
+        uint upcomingTokenId = _nextTokenId();
+        bytes32 seed = keccak256(abi.encodePacked(upcomingTokenId, block.prevrandao, blockhash(block.number - 1), msg.sender));
+        currentSupplies[itemType] += 1;
+        _tokenItemTypes[upcomingTokenId] = itemType;
+        _tokenSeeds[upcomingTokenId] = seed;
+        _mint(msg.sender, 1);
     }
 
     // Transfers, secondary trading, and on-chain royalty enforcement integration
